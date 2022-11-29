213cfce57380eb597e2b50103361a16e401b5a69
waheec19 <waheechowdhury80@gmail.com>
2022-11-28 20:24:49 -0600

    Update Intern.test.js

--- Develop/test/Intern.test.js
+++ Develop/test/Intern.test.js
@@ -1,19 +1,19 @@
 const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const testValue = "UTDALLAS";
+  const e = new Intern("Drake", 1, "Drake@test.com", testValue);
   expect(e.school).toBe(testValue);
 });
 
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
-  const e = new Intern("Foo", 1, "test@test.com", "UCLA");
+  const e = new Intern("Drake", 1, "Drake@test.com", "UTDALLAS");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get school via getSchool()", () => {
-  const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const testValue = "UTDALLAS";
+  const e = new Intern("Drake", 1, "Drake@test.com", testValue);
   expect(e.getSchool()).toBe(testValue);
 });
